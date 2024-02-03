e96a0505f6c246899a727db925f659c840d48820
abhis <abhis@LAPTOP-F84N4T0O>
2024-02-03 18:29:50 +0530

    First commit..

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi welcome to GitHub");
+		System.out.println("You can practice the demo now");
+
+	}
+
+}
